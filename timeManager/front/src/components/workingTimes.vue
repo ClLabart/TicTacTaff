7c1c16fa2256266d4326999256bd581e737f77ad
francois <langfrancoisfrederic@gmail.com>
2023-10-27 20:18:10 +0200

    WIP logic with month and year but without refresh month

--- timeManager/front/src/components/workingTimes.vue
+++ timeManager/front/src/components/workingTimes.vue
@@ -1,19 +1,30 @@
 <template>
-  <div>
-    <select v-model="selectedYear" @change="getWorkingYearTimes()">
-      <option v-for="(year, indexYear) in generateYears" :key="indexYear">
-        {{ year }}
-      </option>
-    </select>
-    <select v-model="selectedMonth" @change="getWorkingMonthTimes()">
-      <option v-for="(month, indexMonth) in chartDataYear.labels" :key="indexMonth">
-        {{ month }}
-      </option>
-    </select>
-  </div>
-
-  <div v-if="dataLoaded">
-    <bar-chart :chart-data="chartDataYear" :options="options" />
+  <div
+      v-if="dataLoaded"
+      class="flex"
+  >
+    <div class="flex-1 p-2">
+      <select
+          v-model="selectedYear"
+          @change="getWorkingYearTimes()"
+      >
+        <option v-for="(year, indexYear) in generateYears" :key="indexYear">
+          {{ year }}
+        </option>
+      </select>
+      <bar-chart :chart-data="chartDataYear" :options="options" :key="selectedYear"/>
+    </div>
+    <div class="flex-1 p-2">
+      <select
+          v-model="selectedMonth"
+          @change="getWorkingMonthTimes()"
+      >
+        <option v-for="(month, indexMonth) in chartDataYear.labels" :key="indexMonth">
+          {{ month }}
+        </option>
+      </select>
+      <bar-chart :chart-data="chartDataMonth" :options="options" :key="selectedMonth"/>
+    </div>
   </div>
 </template>
 
@@ -37,6 +48,16 @@ export default {
             label: "Data One",
             backgroundColor: "#f87979",
             data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+          }
+        ]
+      },
+      chartDataForMonth: {
+        labels: [],
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: []
           }
         ]
       },
@@ -70,12 +91,52 @@ export default {
     },
     chartDataYear () {
       return this.chartDataForYear
+    },
+    chartDataMonth () {
+      return this.chartDataForMonth
+    },
+    weeksInSelectedMonth() {
+      const weeks = [];
+      const monthIndex = this.monthNames.indexOf(this.selectedMonth);
+      let currentDate = moment().year(this.selectedYear).month(monthIndex).startOf('month');
+      while (currentDate.month() === monthIndex) {
+        let startOfWeek = currentDate.clone().startOf('week');
+        let endOfWeek = currentDate.clone().endOf('week');
+        if(startOfWeek.month() !== monthIndex) {
+          startOfWeek = currentDate;
+        }
+        if(endOfWeek.month() !== monthIndex) {
+          endOfWeek = currentDate.clone().endOf('month');
+        }
+        if(startOfWeek.month() !== monthIndex && endOfWeek.month() === monthIndex) {
+          weeks.push(`Semaine du ${startOfWeek.date()} ${this.monthNames[startOfWeek.month()]} au ${endOfWeek.date()} ${this.monthNames[monthIndex]}`);
+        } else if (startOfWeek.month() === monthIndex && endOfWeek.month() !== monthIndex) {
+          weeks.push(`Semaine du ${startOfWeek.date()} ${this.monthNames[monthIndex]} au ${endOfWeek.date()} ${this.monthNames[endOfWeek.month()]}`);
+        } else {
+          weeks.push(`Semaine du ${startOfWeek.date()} au ${endOfWeek.date()} ${this.monthNames[monthIndex]}`);
+        }
+        currentDate.add(1, 'week');
+      }
+      return weeks;
+    },
+    monthNames() {
+      return ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+    }
+  },
+
+  watch: {
+    selectedMonth: {
+      handler(newValue, oldValue) {
+        this.chartDataForMonth.labels = this.weeksInSelectedMonth;
+      },
+      immediate: true
     }
   },
 
   async mounted () {
-  await this.getWorkingYearTimes()
-  this.dataLoaded = true
+    await this.getWorkingYearTimes()
+    await this.getWorkingMonthTimes()
+    this.dataLoaded = true
   },
 
   methods: {
@@ -103,8 +164,24 @@ export default {
         allHoursOfMonth.push(await this.estimationHoursInMonth(startMonth, endMonth))
       }
       this.chartDataForYear.datasets[0].data = allHoursOfMonth
+      return hoursForYear
     },
     async getWorkingMonthTimes () {
+      const start = new Date(this.selectedYear, this.monthNames.indexOf(this.selectedMonth), 1)
+      const end = new Date(this.selectedYear, this.monthNames.indexOf(this.selectedMonth) + 1, 0)
+      const allTimes = await this.getWorkingTimes(start, end)
+      let hoursForMonth = 0
+      let allHoursOfWeek = []
+      for (const element of allTimes.data) {
+        hoursForMonth += this.estimationHours(element)
+      }
+      for (let i = 0; i < this.weeksInSelectedMonth.length; i++) {
+        const startWeek = new Date(this.selectedYear, this.monthNames.indexOf(this.selectedMonth), i * 7 + 1)
+        const endWeek = new Date(this.selectedYear, this.monthNames.indexOf(this.selectedMonth), (i + 1) * 7)
+        allHoursOfWeek.push(await this.estimationHoursInWeek(startWeek, endWeek))
+      }
+      this.chartDataForMonth.datasets[0].data = allHoursOfWeek
+      return hoursForMonth
     },
     async estimationHoursInMonth (start, end) {
       const allTimes  = await this.getWorkingTimes(start, end)
@@ -113,6 +190,14 @@ export default {
         hoursForMonth += this.estimationHours(element)
       }
       return hoursForMonth
+    },
+    async estimationHoursInWeek (start, end) {
+      const allTimes  = await this.getWorkingTimes(start, end)
+      let hoursForWeek = 0
+      for (const element of allTimes.data) {
+        hoursForWeek += this.estimationHours(element)
+      }
+      return hoursForWeek
     },
     estimationHours (data) {
       const startMoment = moment(data.start, 'YYYY-MM-DDTHH:mm:ssZ').valueOf()

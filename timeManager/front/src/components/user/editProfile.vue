a997de8e33dd3ca23dcd465f934da0856da2a790
francois <langfrancoisfrederic@gmail.com>
2023-11-06 14:58:45 +0100

    WIP first commit

--- timeManager/front/src/components/user/editProfile.vue
+++ timeManager/front/src/components/user/editProfile.vue
@@ -6,6 +6,84 @@
     <form @submit.prevent="" class="flex flex-col gap-5">
       <div>
         <label
+            for="firstname"
+            class="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Prénom
+        </label>
+        <div class="relative mt-2 rounded-md shadow-sm">
+          <div
+              class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
+          >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="{1.5}"
+                stroke="currentColor"
+                className="w-3 h-3"
+                style="margin-left: -0.3em"
+                height="1em"
+            >
+              <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+              />
+            </svg>
+          </div>
+
+          <input
+              type="text"
+              name="firstname"
+              id="firstname"
+              class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="prénom"
+              v-model="firstname"
+          />
+        </div>
+      </div>
+      <div>
+        <label
+            for="lastname"
+            class="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Nom
+        </label>
+        <div class="relative mt-2 rounded-md shadow-sm">
+          <div
+              class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
+          >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="{1.5}"
+                stroke="currentColor"
+                className="w-3 h-3"
+                style="margin-left: -0.3em"
+                height="1em"
+            >
+              <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+              />
+            </svg>
+          </div>
+
+          <input
+              type="text"
+              name="lastname"
+              id="lastname"
+              class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Nom"
+              v-model="lastname"
+          />
+        </div>
+      </div>
+      <div>
+        <label
             for="pseudonyme"
             class="block text-sm font-medium leading-6 text-gray-900"
         >
@@ -188,6 +266,8 @@ export default {
 
   data() {
     return {
+      firstname: "",
+      lastname: "",
       username: "",
       email: "",
       password: "",
@@ -220,6 +300,8 @@ export default {
       this.username = this.getUser.data.username;
       this.email = this.getUser.data.email;
       this.id = this.getUser.data.id;
+      this.firstname = this.getUser.data.firstname;
+      this.lastname = this.getUser.data.lastname;
   },
 
 
@@ -243,6 +325,9 @@ export default {
           users: {
             username: this.username,
             email: this.email,
+            password: this.password,
+            firstname: this.firstname,
+            lastname: this.lastname,
           }
 
         };

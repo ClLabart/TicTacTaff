06dca73f35d7e954a723ae2fdecf93a3b36a07ef
Crackita <91308668+Crackita@users.noreply.github.com>
2023-11-12 18:39:31 +0100

    merge

--- timeManager/front/src/components/sidebarComponent.vue
+++ timeManager/front/src/components/sidebarComponent.vue
@@ -14,7 +14,9 @@
             </div>
         </section>
 
-        <section class="flex flex-1 w-fit m-auto md:justify-center md:m-0 md:w-full md:block md:gap-3">
+        <section
+            class="flex flex-1 w-fit m-auto md:justify-center md:m-0 md:w-full md:block md:gap-3"
+        >
             <article class="md:mt-5 flex md:block">
                 <p class="hidden md:block text-[#405D9F] mb-1 px-4">Menu</p>
                 <ul class="flex md:block sm:gap-3">
@@ -38,7 +40,10 @@
                                 fill="black"
                             />
                         </svg>
-                        <p class="md:block" :class="expanded ? 'block navbarNames' : 'hidden'">
+                        <p
+                            class="md:block"
+                            :class="expanded ? 'block navbarNames' : 'hidden'"
+                        >
                             Accueil
                         </p>
                     </li>
@@ -61,7 +66,10 @@
                                 fill="black"
                             />
                         </svg>
-                        <p class="md:block" :class="expanded ? 'block navbarNames' : 'hidden'">
+                        <p
+                            class="md:block"
+                            :class="expanded ? 'block navbarNames' : 'hidden'"
+                        >
                             Pointeuse
                         </p>
                     </li>
@@ -87,7 +95,10 @@
                                 fill="black"
                             />
                         </svg>
-                        <p class="md:block" :class="expanded ? 'block navbarNames' : 'hidden'">
+                        <p
+                            class="md:block"
+                            :class="expanded ? 'block navbarNames' : 'hidden'"
+                        >
                             Statistiques
                         </p>
                     </li>
@@ -124,7 +135,10 @@
                                 d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
                             />
                         </svg>
-                        <p class="md:block" :class="expanded ? 'block navbarNames' : 'hidden'">
+                        <p
+                            class="md:block"
+                            :class="expanded ? 'block navbarNames' : 'hidden'"
+                        >
                             Equipe
                         </p>
                     </li>
@@ -149,7 +163,10 @@
                                 fill="black"
                             />
                         </svg>
-                        <p class="md:block" :class="expanded ? 'block navbarNames' : 'hidden'">
+                        <p
+                            class="md:block"
+                            :class="expanded ? 'block navbarNames' : 'hidden'"
+                        >
                             Profil
                         </p>
                     </li>
@@ -167,7 +184,10 @@
                                 fill="black"
                             />
                         </svg>
-                        <p class="md:block" :class="expanded ? 'block navbarNames' : 'hidden'">
+                        <p
+                            class="md:block"
+                            :class="expanded ? 'block navbarNames' : 'hidden'"
+                        >
                             Déconnexion
                         </p>
                     </li>
@@ -238,31 +258,15 @@ export default {
                 clearTimeout(this.delay);
                 this.expanded = false;
             }
-        }
+        },
+        async removeUserLocalStorage(){
+            localStorage.removeItem("user");
+        },
+        async userDeconnection(){
+            await this.removeUserLocalStorage();
+            location.reload();
+        },
     },
-    async toggleShow(type, childrenType) {
-      this.show = !this.show;
-      await this.setComponent(type, childrenType, this.show);
-    },
-    async setComponent(type, childrenType, show) {
-      const payload = { type, show };
-      if (childrenType) {
-        await this.getUser(this.currentUser.id);
-        payload.childrenType = childrenType;
-      }
-      this.showComponent(payload);
-    },
-
-    async removeUserLocalStorage(){
-        localStorage.removeItem("user");
-    },
-
-    async userDeconnection(){
-        await this.removeUserLocalStorage();
-        location.reload();
-    },
-    
-  }
 };
 </script>
 
